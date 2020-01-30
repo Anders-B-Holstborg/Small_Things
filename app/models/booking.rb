4fd932e7738c2edb4b69c121dbaf90827140cdab
Anders Holstborg <jarmammail@gmail.com>
2020-01-30 21:01:51 +0100

    Added has_many :reviews to Booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,6 @@ class Booking < ApplicationRecord
   ALLOWED_STATUSES = %w[completed offered].freeze
   belongs_to :user
   belongs_to :activity
+  has_many :reviews
   validates :status, inclusion: { in: ALLOWED_STATUSES }
 end
