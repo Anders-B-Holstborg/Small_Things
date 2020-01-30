96aef7c37423a291830b2d00616513257ec94e24
Anders Holstborg <jarmammail@gmail.com>
2020-01-30 21:00:27 +0100

    Added frozen array of statuses to booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
+  ALLOWED_STATUSES = %w[completed offered].freeze
   belongs_to :user
   belongs_to :activity
+  validates :status, inclusion: { in: ALLOWED_STATUSES }
 end
