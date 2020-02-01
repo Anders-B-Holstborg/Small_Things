0cf39a7e37f95dc8db136ffcdbc6765c1637fe31
Anders Holstborg <jarmammail@gmail.com>
2020-02-01 10:42:56 +0100

    Added category_user Rels + Vals

--- app/models/category_user.rb
+++ app/models/category_user.rb
@@ -1,3 +1,12 @@
 class CategoryUser < ApplicationRecord
   belongs_to :user
+  belongs_to :category
+  validates :user_category_preference, presence: true
+  validates :time_length_preference, numericality: { greater_than_or_equal_to: 0 }
+
+  private
+
+  def increase_counter
+    self.offered_counter += 1
+  end
 end
