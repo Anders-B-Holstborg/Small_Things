7ef00172b94932f847c5680f5925a61c837841b5
Anders Holstborg <jarmammail@gmail.com>
2020-02-01 13:53:57 +0100

    Updated routes to match Devise-preferences and removed useless User-views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,4 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:new, :create]
-
-  def index
-  end
-
-  def new
-    if current_user
-      redirect_to #Dashboard, to be implemented
-    end
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def show
-  end
-
-  def delete
-  end
-
   def dashboard
-
   end
 end
