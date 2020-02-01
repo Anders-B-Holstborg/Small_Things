db36f5e3b4a29248541cc06191c9c5c69ec49a80
Arthur DeMeyer <arthur.demeyer75@gmail.com>
2020-02-01 11:23:46 +0100

    Geocooooding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_200321) do
+ActiveRecord::Schema.define(version: 2020_02_01_095345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 2020_01_30_200321) do
     t.string "address"
     t.integer "time_of_sending"
     t.string "preferred_day"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
