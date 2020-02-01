586a1aa970ea81c766a97d96201d14ddac721260
Anders Holstborg <jarmammail@gmail.com>
2020-02-01 15:39:02 +0100

    Implemented Reviews-controller logic for New and Create, with nested route giving the id-params for the associated Booking

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,20 @@
 class ReviewsController < ApplicationController
   def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
+    @review.save
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:user_comment, :rating)
   end
 end
