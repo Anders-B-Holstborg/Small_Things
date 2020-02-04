0072dfea1fd3d93a74d30fba1ea8319f0101a74a
Arthur DeMeyer <arthur.demeyer75@gmail.com>
2020-02-04 19:10:41 +0100

    added the begining of installing geocoding and finally got the prefered day into format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.where.not(latitude: nil, longitude: nil)
+  end
+
   def dashboard
     @user = current_user
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     resources :reviews, only: [ :new, :create ]
   end
 
+  get 'users', to: 'users#index'
+
   get 'dashboard', to: 'users#dashboard'
   root to: 'users#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
