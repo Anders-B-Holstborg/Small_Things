fd1fd895af409511b3c700f0ccd2a9c79fe24035
Anders Holstborg <jarmammail@gmail.com>
2020-02-04 20:05:08 +0100

    Added faker gem - requires bundle install

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,11 @@ User.create(name: 'Emily Watford', email: "emilyw@atford.com", time_of_sending: 
 User.create(name: 'TheExpendableWombat', email: "wombat@gmail.au", time_of_sending: Date.parse("2020-02-04 20:40:00"), password: "123456", password_confirmation: '123456')
 User.create(name: 'Bill', email: "bill.hendricks@gloustershire.uk", time_of_sending: Date.parse("2020-02-04 12:00:00"), password: "123456", password_confirmation: '123456')
 puts "#{User.count} users added!"
+
 category_titles = %w["Reading", "Sports", "Lifestyle", "Culture", "Human Contact"]
+
 puts "Creating categories..."
 category_titles.each do |title|
   Category.create(title: title)
 end
-
 puts "#{category_titles.count} categories added!"
