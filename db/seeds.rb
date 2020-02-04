11d26b7715d92b2bc2e713ec9ae794edcbbc29c1
Anders Holstborg <jarmammail@gmail.com>
2020-02-04 20:00:53 +0100

    added 5 users and 5 categories to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating users..."
+User.create(name: 'dethstriker666', email: "timmy@gmail.com", time_of_sending: Date.parse("2020-02-04 18:25:00"), password: "123456", password_confirmation: '123456')
+User.create(name: 'Sammy G', email: "sammy.g@gmail.com", time_of_sending: Date.parse("2020-02-04 15:00:00"), password: "123456", password_confirmation: '123456')
+User.create(name: 'Emily Watford', email: "emilyw@atford.com", time_of_sending: Date.parse("2020-02-04 08:00:00"), password: "123456", password_confirmation: '123456')
+User.create(name: 'TheExpendableWombat', email: "wombat@gmail.au", time_of_sending: Date.parse("2020-02-04 20:40:00"), password: "123456", password_confirmation: '123456')
+User.create(name: 'Bill', email: "bill.hendricks@gloustershire.uk", time_of_sending: Date.parse("2020-02-04 12:00:00"), password: "123456", password_confirmation: '123456')
+puts "#{User.count} users added!"
+category_titles = %w["Reading", "Sports", "Lifestyle", "Culture", "Human Contact"]
+puts "Creating categories..."
+category_titles.each do |title|
+  Category.create(title: title)
+end
+
+puts "#{category_titles.count} categories added!"
