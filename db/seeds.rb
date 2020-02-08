fa21eb30cc38adc48aab0ce67a5d3e28053f929c
Anders Holstborg <jarmammail@gmail.com>
2020-02-08 13:48:13 +0100

    Added seeding for 3 activities linked to 3 categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,49 @@ date_time_parse = ["2020-02-04 18:25:00", "2020-02-04 18:00:00", "2020-02-04 20:
 
 puts "Creating users..."
 10.times do
-  User.create(name: Faker::Name.name, email: Faker::Internet.email, time_of_sending: Date.parse(date_time_parse.sample), password: "123456", password_confirmation: '123456')
+  User.create!(name: Faker::Name.name, email: Faker::Internet.email, time_of_sending: Date.parse(date_time_parse.sample), password: "123456", password_confirmation: '123456')
 end
 puts "#{User.count} users added!"
 
-category_titles = %w[Reading Sports Lifestyle Culture Human Contact Family]
+#  Lifestyle Culture Human-Contact Family Learning
+category_titles = %w[Reading Sports Culture]
+
+activities = {
+  Reading: [
+    { title: "Newspaper", description: "Read a newspaper of your choice today", duration: 15 },
+    { title: "Book", description: "Read a chapter of an ongoing book today", duration: 20 },
+    { title: "Blog", description: "Read one entry in a blog of your choice", duration: 15 }
+  ],
+  Sports: [
+    { title: "Run", description: "Go for a quick run in the vicinity", duration: 15 },
+    { title: "Bike Ride", description: "Take a bike ride through the local area or an area of your choice", duration: 15 },
+    { title: "Pushups", description: "2-3 sessions of 5-10 pushups", duration: 10 }
+  ],
+  Culture: [
+    { title: "Theatre", description: "A few theatres nearby are doing shows. Time to see one of them!", duration: 120 },
+    { title: "Arts Exhibition", description: "There is a gallery nearby. Give it a check-out!", duration: 60 },
+    { title: "Comics", description: "Read a classic comic of your choice", duration: 30 }
+  ]
+}
 
 puts "Creating categories..."
+@added_cats = 0
 category_titles.each do |title|
-  Category.create(title: title)
+  @category = Category.create(title: title) if Category.where(title: title).empty?
+  @added_cats += 1 if @category
 end
-puts "#{category_titles.count} categories added!"
+puts "#{@added_cats} categories added!"
+
+puts "Adding activities..."
+@added_acts = 0
+activities.each do |key, value|
+  @category = Category.find_by(title: key)
+  value.each do |activity_args|
+    activity_args = activity_args.merge(category_id: @category.id)
+    @acts = Activity.create(activity_args)
+    @added_acts += 1
+  end
+end
+
+puts "We did it fam. Added #{@added_acts} activities!"
+puts "End of the line!"
