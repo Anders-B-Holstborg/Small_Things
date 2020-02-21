21ea8a1fae3e0f6ab2c94141f7ea25ce9fd5ca76
Arthur DeMeyer <arthur.demeyer75@gmail.com>
2020-02-21 12:37:15 +0100

    weather API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,9 @@ class UsersController < ApplicationController
       quote = JSON.parse(user_serialized)
       @quote = quote["quoteText"]
       @quote_author = quote["quoteAuthor"]
+      options = { units: "metric", APPID: "946bfbf42b2767701ba937cb4802fc37" }
+      weather = OpenWeather::Current.city_id("2800867", options)
+      @weather = weather["weather"]
     end
   end
 end
