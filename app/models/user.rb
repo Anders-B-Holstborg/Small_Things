8bac88c07b48e8fa9ad6eee3b97b0e141debc58a
Anders Holstborg <jarmammail@gmail.com>
2020-02-22 16:12:38 +0100

    Changed Bookings-logic. Several bookings with the same user/activity ID can now exist. The controller will check if the activity of the booking has been offered less than the highest amount of offered activities - if all are equal, a random new one will be sent. This way, we can track the *amount* of Bookings for our gamifications, rather than the existance of them. This might bug something somewhere, but it seems to work.

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,56 +32,52 @@ class User < ApplicationRecord
     @user_activity_two = offer_activity
     @booking_one = create_booking(@user_activity_one)
     @booking_two = create_booking(@user_activity_two)
-    UserMailer.with({user: self, activity_one: @user_activity_one, activity_two: @user_activity_two, booking_one: @booking_one, booking_two: @booking_two}).offer_activities.deliver_now
+    UserMailer.with(
+      {user: self, activity_one: @user_activity_one, activity_two: @user_activity_two, booking_one: @booking_one, booking_two: @booking_two}
+    ).offer_activities.deliver_now
   end
 
   def offer_activity
     found_category = find_category(@user_categories)
-
     find_activity(found_category)
   end
 
   def find_category(categories)
-    rolled_category = categories.sample
-    rolled_category = rolled_category.category
+    rolled_categories = categories.sample
+    rolled_category = rolled_categories.category
     if @activity_one
       while rolled_category == @activity_one.category
-        rolled_category = categories.sample
-        rolled_category = rolled_category.category
+        rolled_categories = categories.sample
+        rolled_category = rolled_categories.category
       end
     end
     return rolled_category
   end
 
   def find_activity(category)
-    offered_activity = category.activities.sample
-    while check_activity_validity(offered_activity)
-      category = find_category(@user_categories)
-      offered_activity = category.activities.sample
+    rolled_activity = category.activities.sample
+    unless activity_offerable?(category, rolled_activity)
+      rolled_activity = category.activities.sample
     end
-    return offered_activity
+    return rolled_activity
   end
 
-  def check_activity_validity(activity)
-    self.bookings.find_by(activity_id: activity.id).present?
-  end
-
-  def all_activities_offered?(category)
-    offered_category_activities_amount = 0
-    self.bookings each do |booking|
-      offered_category_activities_amount += 1 if booking.activity.category.id == category.id
+  def activity_offerable?(category, activity)
+    @user_bookings = self.bookings.where(activity: category.activities)
+    activities_hash = Hash.new(0)
+    @user_bookings.each do |booking|
+      activity = booking.activity
+      activities_hash[activity] += 1
     end
-    category.activities.count == offered_category_activities_amount
+    highest_offered_amount = activities_hash.max_by {|_k,v| v}[1]
+    lowest_offered_amount = activities_hash.min_by {|_k,v| v}[1]
+    lowest_offered_amount != highest_offered_amount ? activities_hash[activity] == highest_offered_amount : true
   end
 
   def create_booking(activity)
-    @user = self
-    @booking = Booking.find_by(user_id: @user.id, activity_id: activity.id)
-    unless @booking
-      @booking = Booking.new(user_id: @user.id, activity_id: activity.id)
-      @booking.offered
-      @booking.save!
-    end
+    @booking = Booking.new(user_id: self.id, activity_id: activity.id)
+    @booking.offered
+    @booking.save!
     return @booking
   end
 end
