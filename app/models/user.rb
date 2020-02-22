5eef160d33178ae2b3080eadf081a046de9324b5
Anders Holstborg <jarmammail@gmail.com>
2020-02-22 12:05:17 +0100

    Implemented Bookings-logic so that the same activity doesn't show up twice (atm it can reach a point where no activities can be found)

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,20 +28,30 @@ class User < ApplicationRecord
 
   def send_activity_email
     @user_categories = self.user_categories.where(user_category_preference: true).to_a
-    @user_activity_one = find_activity(@user_categories)
-    @user_activity_two = find_activity(@user_categories)
+    @user_activity_one = find_category(@user_categories)
+    @user_activity_two = find_category(@user_categories)
     @booking_one = create_booking(@user_activity_one)
     @booking_two = create_booking(@user_activity_two)
-    UserMailer.with(
-      {user: self, activity_one: @user_activity_one, activity_two: @user_activity_two, booking_one: @booking_one, booking_two: @booking_two}
-    ).offer_activities.deliver_now
+    UserMailer.with({user: self, activity_one: @user_activity_one, activity_two: @user_activity_two, booking_one: @booking_one, booking_two: @booking_two}).offer_activities.deliver_now
   end
 
-  def find_activity(user_categories)
+  def find_category(user_categories)
     rolled_user_category = user_categories.sample
     rolled_category = rolled_user_category.category
     @user_categories.delete(rolled_user_category)
-    rolled_category.activities.sample
+    find_activity(rolled_category)
+  end
+
+  def find_activity(category)
+    offered_activity = category.activities.sample
+    while check_activity_validity(offered_activity)
+      offered_activity = category.activities.sample
+    end
+    return offered_activity
+  end
+
+  def check_activity_validity(activity)
+    self.bookings.find_by(activity_id: activity.id).present?
   end
 
   def create_booking(activity)
