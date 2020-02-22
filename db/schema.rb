4244a99deeb5dcb106437ec8a78a495d4f442c6b
Arthur DeMeyer <arthur.demeyer75@gmail.com>
2020-02-22 11:52:14 +0100

    I removed the address column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_154424) do
+ActiveRecord::Schema.define(version: 2020_02_22_103538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +96,6 @@ ActiveRecord::Schema.define(version: 2020_02_01_154424) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "address"
     t.datetime "time_of_sending"
     t.datetime "preferred_day"
     t.float "latitude"
