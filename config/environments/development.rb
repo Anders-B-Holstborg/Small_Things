1c9e5f84ed4b3bdbca9fff70b0f94a4f87293560
Anders Holstborg <jarmammail@gmail.com>
2020-02-27 17:01:49 +0100

    Added a welcome mail function when a user signs up. The mail is not designed, but the functionality works.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,9 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
-  config.action_mailer.asset_host = "http://localhost:3000"
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  #config.action_mailer.asset_host = "http://localhost:3000"
+  #config.action_mailer.delivery_method = :letter_opener
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
